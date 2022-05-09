13556267518edfd41357f1f80e98a026888bf503
JiahuiSun <jhsun1997@sjtu.edu.cn>
2022-05-09 00:35:42 +0000

    no init param

--- main.py
+++ main.py
@@ -54,18 +54,18 @@ def main(args):
         return Independent(Normal(*logits), 1)
 
     th.nn.init.constant_(actor.sigma_param, -0.5)
-    for m in list(actor.modules()) + list(critic.modules()):
-        if isinstance(m, th.nn.Linear):
-            # orthogonal initialization
-            th.nn.init.orthogonal_(m.weight, gain=np.sqrt(2))
-            th.nn.init.zeros_(m.bias)
-    # do last policy layer scaling, this will make initial actions have (close to)
-    # 0 mean and std, and will help boost performances,
-    # see https://arxiv.org/abs/2006.05990, Fig.24 for details
-    for m in actor.mu.modules():
-        if isinstance(m, th.nn.Linear):
-            th.nn.init.zeros_(m.bias)
-            m.weight.data.copy_(0.01 * m.weight.data)
+    # for m in list(actor.modules()) + list(critic.modules()):
+    #     if isinstance(m, th.nn.Linear):
+    #         # orthogonal initialization
+    #         th.nn.init.orthogonal_(m.weight, gain=np.sqrt(2))
+    #         th.nn.init.zeros_(m.bias)
+    # # do last policy layer scaling, this will make initial actions have (close to)
+    # # 0 mean and std, and will help boost performances,
+    # # see https://arxiv.org/abs/2006.05990, Fig.24 for details
+    # for m in actor.mu.modules():
+    #     if isinstance(m, th.nn.Linear):
+    #         th.nn.init.zeros_(m.bias)
+    #         m.weight.data.copy_(0.01 * m.weight.data)
 
     actor_optim = Adam(actor.parameters(), lr=args.lr_actor)
     critic_optim = Adam(list(critic.parameters())+list(cost_critic.parameters()), lr=args.lr_critic)
