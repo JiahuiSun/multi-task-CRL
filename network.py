d028eac4ce3185fac9c403a271cd9fed051ec875
JiahuiSun <jhsun1997@sjtu.edu.cn>
2022-05-11 08:19:18 +0000

    change basenet

--- network.py
+++ network.py
@@ -8,39 +8,23 @@ class BaseNet(nn.Module):
     def __init__(
         self,
         state_shape,
-        taskid_dim,
-        n_encoder=10
+        taskid_dim
     ) -> None:
         super().__init__()
-        input_dim = int(np.prod(state_shape))
-        self.state_encoder_list = nn.ModuleList([
-            nn.Sequential(
-                nn.Linear(input_dim, 128),
-                nn.Tanh()
-            )
-            for _ in range(n_encoder)
-        ])
-        self.task_encoder = nn.Sequential(
-            nn.Linear(taskid_dim, 128),
-            nn.Tanh()
-        )
-        self.mlp = nn.Sequential(
-            nn.Linear(128, 128),
+        input_dim = int(np.prod(state_shape)) + taskid_dim
+        self.fc = nn.Sequential(
+            nn.Linear(input_dim, 256),
+            nn.Tanh(),
+            nn.Linear(input_dim, 256),
+            nn.Tanh(),
+            nn.Linear(256, 256),
             nn.Tanh()
         )
         self.output_dim = 256
     
     def forward(self, obs, taskid):
-        state_embs = th.stack([state_encoder(obs) for state_encoder in self.state_encoder_list], dim=0)  # KBF
-        state_embs = state_embs.permute(1, 0, 2)  # BKF
-        task_emb = self.task_encoder(taskid)  # BF
-        task_emb_nograd = task_emb.detach()
-        atten_logit = state_embs.matmul(task_emb_nograd.unsqueeze(-1))  # BK1
-        atten_weight = F.softmax(atten_logit, dim=1)
-        state_emb = atten_weight.permute(0, 2, 1).matmul(state_embs).squeeze(1)  # BF
-        state_emb = self.mlp(state_emb)
-        state_task_emb = th.cat([task_emb, state_emb], dim=1)  # B(2F)
-        return state_task_emb
+        obs_task = th.cat([obs, taskid], dim=1)
+        return self.fc(obs_task)
 
 
 class Actor(nn.Module):
